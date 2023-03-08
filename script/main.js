967bd646ce161fba775efe736c12dcb0c0ae59ea
Dov Rubinstein <dovberr@gmail.com>
2023-03-08 09:15:32 +0200

    milestone7

--- script/main.js
+++ script/main.js
@@ -1,4 +1,65 @@
 "use strict";
+
+class StockSearch {
+  constructor(showResultList) {
+    this.baseUrL = baseUrL;
+    this.getInput = document.getElementById("input");
+    this.getSearchBtn = document.getElementById("search");
+    this.showResultList = showResultList;
+    this.loadingSymbol = document.getElementById("loading");
+    this.errorsContainer = document.getElementById("errors");
+
+    this.getSearchBtn.addEventListener("click", () => this.getStockPrices());
+  }
+
+  loaderIndicator() {
+    this.loadingSymbol.classList.toggle("visually-hidden");
+  }
+
+  async getStockPrices() {
+    this.loaderIndicator();
+    try {
+      let response = await fetch(
+        `${this.baseUrL}/api/v3/search?query=${this.getInput.value}&limit=10&exchange=NASDAQ`
+      );
+      let data = await response.json();
+      this.makeResultList(data);
+    } catch (error) {
+      this.errorsContainer.innerHTML = `Error: ${error.message}`;
+    } finally {
+      this.loaderIndicator();
+    }
+  }
+
+  highlightMatches(text, query) {
+    const regex = new RegExp(query, "gi");
+    return text.replace(regex, match => `<mark id="mark">${match}</mark>`);
+  }
+
+  makeResultList(data) {
+    this.showResultList.innerHTML = "";
+    const companyPage = "./company.html?symbol=";
+    for (const company of data) {
+      const companyInfoUrl = `${this.baseUrL}/api/v3/company/profile/${company.symbol}`;
+      fetch(companyInfoUrl)
+        .then((resp) => resp.json())
+        .then((data) => {
+          let percentage = data.profile.changesPercentage;
+          let image = data.profile.image;
+          const aCompany = document.createElement("div");
+          const name = this.highlightMatches(company.name, this.getInput.value);
+          const symbol = this.highlightMatches(`(${company.symbol})`, this.getInput.value);
+          aCompany.innerHTML = `<a href=${companyPage}${company.symbol} target="_blank"><div>${name} ${symbol} (${percentage}) <img src="${image}"</div></a>`;
+          this.showResultList.appendChild(aCompany);
+        });
+    }
+  }
+}
+
+const stockSearch = new StockSearch(document.getElementById("result-list"));
+
+
+/* "use strict";
 //using OOP methodology
 
 class StockSearch {
@@ -49,7 +110,7 @@ class StockSearch {
     }
   }
 }
-const stockSearch = new StockSearch(document.getElementById("result-list"));
+const stockSearch = new StockSearch(document.getElementById("result-list")); */
 
 //using function methodology
 /* const getInput = document.getElementById("input");
